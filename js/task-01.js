2d9dc5fc3340f70ee138b40d2a1c44f297c81609
Vitos1992 <125671703+Vitos1992@users.noreply.github.com>
2023-09-18 16:17:02 +0300

    оновлене

---
+++ js/task-01.js
@@ -0,0 +1,16 @@
+console.log(document);
+
+const body = document.body;
+console.log(body);
+
+const categories = document.querySelector(".categories");
+console.log(categories);
+
+//const firstListChild = categories.firstElementChild;
+//console.log(firstListChild);
+
+//const lastListChild = categories.lastElementChild;
+//console.log(lastListChild);
+
+//const listItem = categories.children;
+//console.log(listItem);
