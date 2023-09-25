83c5dd098c52f0bd94a8dcdba78c8421c4daedd4
Vitos1992 <125671703+Vitos1992@users.noreply.github.com>
2023-09-25 16:14:53 +0300

    task-04

--- js/task-04.js
+++ js/task-04.js
@@ -1,5 +1,18 @@
-const counterValue = 0;
-const btn = document.querySelector("#value");
+let counterValue = 0;
+const valueBtn = document.getElementById("value");
 
-const btnDecrement = document.querySelector();
-const btnIncrement = document.querySelector();
+const btnDecrement = document.querySelector('[data-action="decrement"]');
+const btnIncrement = document.querySelector('[data-action="increment"]');
+
+const decrementOnClick = (event) => {
+  counterValue -= 1;
+  valueBtn.textContent = counterValue;
+};
+
+const incrementOnClick = (event) => {
+  counterValue -= 1;
+  valueBtn.textContent = counterValue;
+};
+
+btnDecrement.addEventListener("click", decrementOnClick);
+btnIncrement.addEventListener("click", incrementOnClick);
