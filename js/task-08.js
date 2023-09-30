06a85531aafb8f7b0838fed533fc5ca0474efb3f
Vitos1992 <125671703+Vitos1992@users.noreply.github.com>
2023-09-30 13:17:44 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const logForm = document.querySelector(".login-form");
+logForm.addEventListener("submit", logFormSubmit);
 
+function logFormSubmit(event) {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+
+  if (!email.value.trim() || !password.value.trim()) {
+    return alert("Please fill in all the fields!");
+  } else {
+    const formData = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(formData);
+    event.currentTarget.reset();
+  }
+}
